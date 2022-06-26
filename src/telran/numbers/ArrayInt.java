9de26c14678dc92985aa9a3715afef8a3b9cfff8
User <User@SuperMicro>
2022-06-26 12:35:02 +0300

    adds: bubble sort code optimization, update binaryIndexOf code, code beautified

--- src/telran/numbers/ArrayInt.java
+++ src/telran/numbers/ArrayInt.java
@@ -9,14 +9,13 @@ public class ArrayInt {
 	 * @param number
 	 * @return new array with added number
 	 */
-	public static int [] addNumber(int [] ar, int number) {
-		int [] res = Arrays.copyOf(ar, ar.length+1);
-		res [res.length-1] = number;
+	public static int[] addNumber(int[] ar, int number) {
+		int[] res = Arrays.copyOf(ar, ar.length + 1);
+		res[res.length - 1] = number;
 		return res;
-		
+
 	}
-		
-	
+
 	/**
 	 * 
 	 * @param ar
@@ -24,15 +23,14 @@ public class ArrayInt {
 	 * @param number
 	 * @return new array containing number value at the given index
 	 */
-	    public static int [] insertNumber(int ar[], int index, int number) {
-		int res []  = new int [ar.length + 1];
+	public static int[] insertNumber(int ar[], int index, int number) {
+		int res[] = new int[ar.length + 1];
 		System.arraycopy(ar, 0, res, 0, index);
-		res [index] = number;
+		res[index] = number;
 		System.arraycopy(ar, index, res, index + 1, ar.length - index);
-		
+
 		return res;
 	}
-
 
 	/**
 	 * 
@@ -40,22 +38,24 @@ public class ArrayInt {
 	 * @param index
 	 * @return new array with removed number at the given index
 	 */
-	public static int[] removeNumber(int []ar, int index) {
-	
-		int [] res = new int [ar.length-1];
+	public static int[] removeNumber(int[] ar, int index) {
+
+		int[] res = new int[ar.length - 1];
 		System.arraycopy(ar, 0, res, 0, index);
-		System.arraycopy(ar, index+1, res, index, res.length - index);
+		System.arraycopy(ar, index + 1, res, index, res.length - index);
 		return res;
 	}
+
 	/**
 	 * 
 	 * @param ar
 	 * @param number
-	 * @return index of value if given number of first occurrence exist in array or -1 if doesn't exist
+	 * @return index of value if given number of first occurrence exist in array or
+	 *         -1 if doesn't exist
 	 */
-	public static int indexOf (int ar[], int number) {
+	public static int indexOf(int ar[], int number) {
 		int res = -1;
-		for (int i = 0; i < ar.length ; i++) {
+		for (int i = 0; i < ar.length; i++) {
 			if (ar[i] == number) {
 				res = i;
 				break;
@@ -64,49 +64,47 @@ public class ArrayInt {
 		return res;
 	}
 
-
 //	public static void sort(int[] ar) {
 //			for (int i = 0; i < ar.length; i++) {
 //			moveMaxToEnd(ar);
 //			}
 //			
 //		}
-		
-		//Improve algorithm with a proper move MaxToEnd method call
-	
-		public static void sort(int[] ar) { // Improved algorithm with a proper move MaxToEnd method call
-			boolean res = false;
-			do {
-				res = moveMaxToEnd(ar);
-			} while (res != true);
 
-		}
+	// Improve algorithm with a proper move MaxToEnd method call
 
-		/**
-		 * 
-		 * @param ar
-		 * @return Method created as homework task with additional var as flag if array
-		 *         is already sorted and no need to call method moveMaxToEnd again
-		 */
-		private static boolean moveMaxToEnd(int[] ar) {
-			boolean flag = true;
-			for (int i = 1; i < ar.length; i++) {
-				if (ar[i - 1] > ar[i]) {
-					swap(ar, i);
-					flag = false;
-				}
+	public static void sort(int[] ar) { // Improved algorithm with a proper move MaxToEnd method call
+		boolean res = false;
+		do {
+			res = moveMaxToEnd(ar);
+		} while (res != true);
 
+	}
+
+	/**
+	 * 
+	 * @param ar
+	 * @return Method created as homework task with additional var as flag if array
+	 *         is already sorted and no need to call method moveMaxToEnd again
+	 */
+	private static boolean moveMaxToEnd(int[] ar) {
+		boolean flag = true;
+		for (int i = 1; i < ar.length; i++) {
+			if (ar[i - 1] > ar[i]) {
+				swap(ar, i);
+				flag = false;
 			}
-			return flag;
-		}
 
-		private static void swap(int[] ar, int index) {
-			int tmp = ar[index - 1];
-			ar[index - 1] = ar[index];
-			ar[index] = tmp;
 		}
-	
-	
+		return flag;
+	}
+
+	private static void swap(int[] ar, int index) {
+		int tmp = ar[index - 1];
+		ar[index - 1] = ar[index];
+		ar[index] = tmp;
+	}
+
 //	public static void sort(int[] ar) {
 //		boolean sort = false;
 //		for (int i = 0; i < ar.length; i++) {
@@ -117,7 +115,6 @@ public class ArrayInt {
 //			else break;
 //		}
 
-
 //	private static void moveMaxToEnd(int[] ar) { // Method written in the classwork
 //
 //		for (int i = 1; i < ar.length; i++) {
@@ -127,16 +124,15 @@ public class ArrayInt {
 //			}
 //		}
 //	}
-	
-
 
 	/**
 	 *
-	 * @param ar - sorted array
+	 * @param ar     - sorted array
 	 * @param number
-	 * @return if the given number exist  in the given array then returns the index of first occurrence, 
-	 * otherwise returns negative value of (index + 1) where index is the index at which position 
-	 * given element should have been
+	 * @return if the given number exist in the given array then returns the index
+	 *         of first occurrence, otherwise returns negative value of (index + 1)
+	 *         where index is the index at which position given element should have
+	 *         been
 	 * 
 	 */
 	public static int binaryIndexOf(int[] ar, int number) {
@@ -145,7 +141,7 @@ public class ArrayInt {
 		int middle = ar.length / 2;
 		int i = 0;
 
-		if (number == ar[i] && number == ar[i + 1]) { //looking if numbers are same, so i = middle = 0
+		if (number == ar[i] && number == ar[i + 1]) { // looking if numbers are same, so i = middle = 0
 			middle = i;
 		}
 		while (left <= right && ar[middle] != number) {
@@ -188,12 +184,5 @@ public class ArrayInt {
 		return middle;
 
 	}
- 	
-	
-	
-	
-	
-	
-	
- 
+
 }
--- src/telran/numbers/tests/ArrayIntTests.java
+++ src/telran/numbers/tests/ArrayIntTests.java
@@ -16,108 +16,105 @@ class ArrayIntTests {
 
 	@Test
 	void referenceTests() {
-		int ar[] = {1,2,3,4};
+		int ar[] = { 1, 2, 3, 4 };
 		int ar1[] = ar;
 		ar1[1] = 100;
 		assertEquals(100, ar[1]);
 	}
-	
+
 	@Test
 	void addNumberTest() {
-		int expected [] = {1,2,-10,20};
-		int ar[] = {1,2, -10};
+		int expected[] = { 1, 2, -10, 20 };
+		int ar[] = { 1, 2, -10 };
 		assertArrayEquals(expected, ArrayInt.addNumber(ar, 20));
 	}
-	
+
 	@Test
 	void insertNumberTest() {
-		int expected [] = {1,2,-10,20};
-		int ar[] = {1,2, 20};
+		int expected[] = { 1, 2, -10, 20 };
+		int ar[] = { 1, 2, 20 };
 		assertArrayEquals(expected, ArrayInt.insertNumber(ar, 2, -10));
 	}
-	
+
 	@Test
 	void removeNumberTest() {
-		int expected1 [] = {1,20};
-		int expected2 [] = {2, 20};
-		int expected3 [] = {1, 2};
-		int ar[] = {1,2, 20};
+		int expected1[] = { 1, 20 };
+		int expected2[] = { 2, 20 };
+		int expected3[] = { 1, 2 };
+		int ar[] = { 1, 2, 20 };
 		assertArrayEquals(expected1, ArrayInt.removeNumber(ar, 1));
 		assertArrayEquals(expected2, ArrayInt.removeNumber(ar, 0));
 		assertArrayEquals(expected3, ArrayInt.removeNumber(ar, 2));
 	}
-	
+
 	@Test
 	void indexOfTest() {
-		int ar[] = {1,2, 20, 2};
-		assertEquals(1,  ArrayInt.indexOf(ar, 2));
-		assertEquals(0,  ArrayInt.indexOf(ar, 1));
-		assertEquals(-1,  ArrayInt.indexOf(ar, 10));
+		int ar[] = { 1, 2, 20, 2 };
+		assertEquals(1, ArrayInt.indexOf(ar, 2));
+		assertEquals(0, ArrayInt.indexOf(ar, 1));
+		assertEquals(-1, ArrayInt.indexOf(ar, 10));
 	}
-	
+
 	@Test
 	void sortSmallTest() {
-		int ar[] = new int [SMALL_N_NUMBERS];
+		int ar[] = new int[SMALL_N_NUMBERS];
 		fillRandomNumbers(ar);
 		ArrayInt.sort(ar);
-		runSortTest(ar);		
+		runSortTest(ar);
 	}
-	
+
 	@Test
-	//TODO HW4
 	void sortLargeTest() {
-		int ar[] = new int [LARGE_N_NUMBERS];
+		int ar[] = new int[LARGE_N_NUMBERS];
 		fillRandomNumbers(ar);
-		//Arrays.sort(ar);
+		// Arrays.sort(ar);
 		ArrayInt.sort(ar);
-		runSortTest(ar);		
+		runSortTest(ar);
 	}
-	
+
 	@Test
 	void binarySearchTest() {
-		int ar[] = {1, 5, 100, 200, 350, 500};
-		assertEquals(0, ArrayInt.binaryIndexOf(ar , 1));
-		assertEquals(2, ArrayInt.binaryIndexOf(ar , 100));
-		assertEquals(5, ArrayInt.binaryIndexOf(ar , 500));
+		int ar[] = { 1, 5, 100, 200, 350, 500 };
+		assertEquals(0, ArrayInt.binaryIndexOf(ar, 1));
+		assertEquals(2, ArrayInt.binaryIndexOf(ar, 100));
+		assertEquals(5, ArrayInt.binaryIndexOf(ar, 500));
 		assertTrue(ArrayInt.binaryIndexOf(ar, 150) < 0); // returns -1 if there is no such element
-		//Tests for HW#4
-		//If there is no such element return index if it will be on which position it should stand
-		assertEquals(-1 ,ArrayInt.binaryIndexOf(ar , -10));
-		assertEquals(-4 ,ArrayInt.binaryIndexOf(ar , 150));
-		assertEquals(-6 ,ArrayInt.binaryIndexOf(ar , 600));
-		int ar1[] = {1,1,1,1,1}; // if same numbers need to return index 0
-		assertEquals (0, ArrayInt.binaryIndexOf(ar1 , 1));
-		}
-	
-	@Test 
+		// Tests for HW#4
+		// If there is no such element return index if it will be on which position it
+		// should stand
+		assertEquals(-1, ArrayInt.binaryIndexOf(ar, -10));
+		assertEquals(-4, ArrayInt.binaryIndexOf(ar, 150));
+		assertEquals(-6, ArrayInt.binaryIndexOf(ar, 600));
+		int ar1[] = { 1, 1, 1, 1, 1 }; // if same numbers need to return index 0
+		assertEquals(0, ArrayInt.binaryIndexOf(ar1, 1));
+	}
+
+	@Test
 	void indexOfPerformance() {
-		int ar[] = new int [LARGE_N_NUMBERS];
-		for (int i = 0 ; i < N_RUNS ; i++) {
+		int ar[] = new int[LARGE_N_NUMBERS];
+		for (int i = 0; i < N_RUNS; i++) {
 			assertTrue(ArrayInt.indexOf(ar, 1) < 0);
 		}
 	}
-	
-	@Test 
+
+	@Test
 	void binaryIndexOfPerformance() {
-		int ar[] = new int [LARGE_N_NUMBERS];
-		for (int i = 0 ; i < N_RUNS ; i++) {
+		int ar[] = new int[LARGE_N_NUMBERS];
+		for (int i = 0; i < N_RUNS; i++) {
 			assertTrue(ArrayInt.binaryIndexOf(ar, 1) < 0);
 		}
 	}
-	
-	private void fillRandomNumbers(int[]ar) {
+
+	private void fillRandomNumbers(int[] ar) {
 		for (int i = 0; i < ar.length; i++) {
-			ar[i] = (int) (Math.random()*Integer.MAX_VALUE);
+			ar[i] = (int) (Math.random() * Integer.MAX_VALUE);
 		}
 	}
-	
-		
+
 	private void runSortTest(int[] ar) {
 		for (int i = 1; i < ar.length; i++) {
 			assertTrue(ar[i - 1] <= ar[i]);
 		}
 	}
-	
-	
 
 }
